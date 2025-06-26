a4ff1c71d5fa50298181a085cec270d4bc0f9671
Tamim <tamimvaiya.123@gmail.com>
2025-06-27 00:38:41 +0600

    added image part, made design better

--- food/models.py
+++ food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default='https://cdn.dribbble.com/userupload/22570626/file/original-379b4978ee41eeb352e0ddacbaa6df96.jpg?resize=752x564&vertical=center')
