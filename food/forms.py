03d52fcd32f9042b1ab082ede2049225c1394756
Tamim <tamimvaiya.123@gmail.com>
2025-06-27 03:45:52 +0600

    improved item-delete ui, item-form ui and added widgets in forms.py

--- food/forms.py
+++ food/forms.py
@@ -5,4 +5,33 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['item_name', 'item_desc', 'item_price', 'item_image']
-        
+
+        widgets = {
+            'item_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter item name',
+                'maxlength': '200',
+            }),
+            'item_desc': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter item description',
+                'maxlength': '200',
+                'rows': 3,
+                'style': 'resize: none;',
+            }),
+            'item_price': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter item price',
+                'min': '0',
+            }),
+            'item_image': forms.URLInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter image URL',
+            }),
+        }
+        labels = {
+            'item_name': 'Item Name',
+            'item_desc': 'Description',
+            'item_price': 'Price',
+            'item_image': 'Image URL',
+        }
